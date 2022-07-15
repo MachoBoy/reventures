5af9a637df70bc37b74491e138936c56755a559d
MachoBoy <jiiwon0@gmail.com>
2022-07-14 21:41:10 -0700

    relocate data

---
+++ pages/data/tips-data.ts
@@ -0,0 +1,31 @@
+export const tipsApplyData = [
+  {
+    icon: '/ico_home.png',
+    title: 'HOMEPAGE',
+    desc: 'TIPS 홈페이지 > APPLY',
+  },
+  {
+    icon: '/ico_email.png',
+    title: 'E-Mail',
+    desc: `자유양식의 사업계획서 또는 IR자료를 첨부하여 지원 \u000A E-Mail.apply@reventures.co.kr | FAX. 0507-0307-2001`,
+  },
+];
+
+export const tipsCategoryData = [
+  {
+    icon: '/ico_tips1.png',
+    title: 'AI / Digital Transformation',
+  },
+  {
+    icon: '/ico_tips2.png',
+    title: 'Bio / Healthcare',
+  },
+  {
+    icon: '/ico_tips3.png',
+    title: 'Platform / Commerce',
+  },
+  {
+    icon: '/ico_tips4.png',
+    title: '부품·소재',
+  },
+];
