90f0fae5faf649aa77cfc478e0c7e37d3f3701e0
MachoBoy <jiiwon0@gmail.com>
2022-07-15 17:53:00 -0700

    header develop

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useScroll } from 'framer-motion';
@@ -6,14 +7,18 @@ import { navData } from '../../pages/data/nav-data';
 
 // Insight 메뉴 추가되면 max-w-[699px]
 const Header = () => {
-  const { scrollY, scrollYProgress } = useScroll();
+  const { scrollY } = useScroll();
   const [scrollPosition, setScrollPosition] = useState(0);
+  const router = useRouter();
+
   useEffect(() => {
     return scrollY.onChange((latest) => {
       setScrollPosition(latest);
       //console.log('Page scroll: ', current);
     });
   }, [scrollY]);
+
+  //console.log(router);
   return (
     <div
       className={`header w-full h-[80px] fixed top-0 left-0 m-auto flex justify-center items-center z-50 ${
@@ -38,12 +43,28 @@ const Header = () => {
           <ul className='menu w-full flex justify-evenly items-center'>
             {navData.map(({ title, link, children }, index) => {
               return (
-                <li key={index}>
+                <li
+                  key={index}
+                  className='flex flex-col justify-center items-center'
+                >
                   <Link href={link}>
-                    <a className='text-lg text-[#959699] hover:text-white'>
+                    <a
+                      className={`text-lg font-semibold ${
+                        link === router.pathname
+                          ? 'text-white'
+                          : 'text-[#959699]'
+                      } hover:text-white`}
+                    >
                       {title}
                     </a>
                   </Link>
+                  <div
+                    className={`mt-3 w-[53px] h-[3px] ${
+                      link === router.pathname
+                        ? 'bg-[#707070]'
+                        : 'bg-transparent'
+                    }`}
+                  ></div>
                 </li>
               );
             })}
